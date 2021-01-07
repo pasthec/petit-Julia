bd13cf1508767e182db7f14940fc25e4e18b4ed6
Thomas Laure <39703028+DarthEluar@users.noreply.github.com>
2021-01-07 15:47:02 +0100

    print ints and bools

--- compile.ml
+++ compile.ml
@@ -5,11 +5,16 @@ open Typer
 
 let rec compile_expr e =
     match e.tdesc with 
-    | Tprint l -> let lc= List.map (fun e -> (compile_expr e) ++ (call "print") ++ (addq (imm 8) !%rsp)) l in 
+    | Tprint l -> let lc= List.map (fun e -> (compile_expr e) ++ (call "print") ++ (addq (imm 16) !%rsp)) l in 
                     List.fold_left (++) nop lc
-    | TEint i -> movq (imm64 i) !%rax ++
-                pushq !%rax (*++
-                pushq (imm 1)*) (*pour dire que c'est un entier*)
+    | TEint i -> pushq (imm64 i) ++
+                pushq (imm 1)
+    | TEbool b -> let i=(if b then 1 else 0) in 
+                pushq (imm i) ++
+                pushq (imm 2)
+
+    (*| TEstring s -> *)
+    
     | _ -> nop 
 
 
@@ -23,14 +28,26 @@ let print_f =
     (*fonction qui affiche la valeur en sommet de pile et la dépile
     Teste le type de la valeur, et appelle la fonction d'affichage dédiée*)
     label "print" ++
-    (*popq rdi ++*)
+    
     (*ici on devra tester la valeur dans rdi, pour l'instant osef j'affiche des entiers*)
     pushq !%rbp ++
     movq !%rsp !% rbp ++
        
-    movq (ind ~ofs:16 rbp) !%rdi ++
+    movq (ind ~ofs:16 rbp) !%rsi ++ (*premier composant : le tag*)
+    movq (ind ~ofs:24 rbp) !%rdi ++ (*deuxième composant : le truc à afficher*)
 
-    call "print_int" ++
+    cmpq (imm 1) !%rsi ++
+    je "print_int" ++
+
+    cmpq (imm 2) !%rsi ++
+    je "print_bool" ++
+
+    (*cmpq (imm 3) !%rsi ++
+    je "print_string"*)
+
+    (*dans les faits, ici il faudra faire un print pour chaque structure*)
+
+    label "end_print" ++
     movq !%rbp !%rsp ++
     popq rbp ++
     movq (imm 0) !%rax ++
@@ -38,25 +55,47 @@ let print_f =
 
 let print_int = 
     label "print_int" ++
-    (*subq (imm 8) !%rsp ++ (* alignement jsp pourquoi, ça marche ni avec ni sans*)*)
+    
    
     movq !%rdi !%rsi ++
     leaq (lab ".Sprint_int") rdi ++
     movq (imm 0) !%rax ++
     call "printf" ++
 
-    (*addq (imm 8) !%rsp ++*)
-    ret
+    jmp "end_print"
 
-let print_data = 
+let print_bool =
+    label "print_bool" ++
+    cmpq (imm 0) !%rdi ++
+    je ".Lfalse" ++
+    leaq (lab ".btrue") rdi ++
+    jmp ".Lprint" ++
+    label ".Lfalse" ++
+    leaq (lab ".bfalse") rdi ++
+    label ".Lprint" ++
+    movq (imm 0) !%rax ++
+    call "printf" ++
+
+    jmp "end_print"
+
+(*let print_string =
+    label "print_string" ++
+    movq !%rdi !%rsi ++
+    leaq (lab ".Sprint_string") rdi ++
+    movq (imm 0) !%rax ++
+    call "printf" ++
+
+    jmp "end_print"*)
+
+let print_data =  (*penser à virer les \n pour ne pas faire doublon avec println quand on aura les strings*)
     label ".Sprint_int" ++
     string "%d\n" ++
-    label ".Sprint_string" ++
-    string "%s" ++
+    (*label ".Sprint_string" ++
+    string "%s" ++*)
     label ".btrue" ++
-    string "true" ++
+    string "true\n" ++
     label ".bfalse" ++
-    string "false" 
+    string "false\n" 
 
 let compile (decls, funs, structsi,vars) ofile =
     let code = List.map compile_instr decls in
@@ -70,6 +109,7 @@ let compile (decls, funs, structsi,vars) ofile =
             ret ++
             print_f ++
             print_int ++
+            print_bool ++
             ins ""; (* on saute une ligne pour aérer *)
           data =
               (*Smap.fold (fun x _ l -> label x ++ (dquad [0]) ++ l) vars*) d
